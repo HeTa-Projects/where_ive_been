206edb4054316ab776c41f19b7779f5339425516
TahaEmreDogan <tahaemredogan@hotmail.com>
2026-07-25 21:43:02 +0300

    fix: resolve TypeScript undefined check in mekanlar page for Vercel build

--- app/mekanlar/[sehirId]/page.tsx
+++ app/mekanlar/[sehirId]/page.tsx
@@ -1,4 +1,4 @@
-import { sehirBul } from "../../gezi-verileri";
+import { sehirBul, sehirler } from "../../gezi-verileri";
 import { MekanRehberiClient } from "./MekanRehberiClient";
 
 export default async function MekanRehberi({
@@ -10,7 +10,7 @@ export default async function MekanRehberi({
 }) {
   const { sehirId } = await params;
   const query = searchParams ? await searchParams : {};
-  const sehir = sehirBul(sehirId);
+  const sehir = sehirBul(sehirId) ?? sehirler[0];
 
   return (
     <MekanRehberiClient
